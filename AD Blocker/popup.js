1c8e958ff9bd83e2d155db2ffd7d26e6f05cd2b0
Anjusha-Reddy <peddireddya@iitbhilai.ac.in>
2020-10-11 15:07:10 +0530

    Added features to popup.js

--- AD Blocker/popup.js
+++ AD Blocker/popup.js
@@ -1,12 +1,39 @@
-window.onload = function() {
-	function updateLabel() {
-		var enabled = chrome.extension.getBackgroundPage().enabled;
-		document.getElementById('toggle_button').value = enabled ? "Disable" : "Enable";
-	}
-	document.getElementById('toggle_button').onclick = function() {
-		var background = chrome.extension.getBackgroundPage();
-		background.enabled = !background.enabled;
-		updateLabel();
-	};
-	updateLabel();
+var incognito;
+var url;
+
+function settingChanged() {
+  var type = this.id;
+  var setting = this.value;
+  var pattern = /^file:/.test(url) ? url : url.replace(/\/[^\/]*?$/, '/*');
+  console.log(type+' setting for '+pattern+': '+setting);
+  chrome.contentSettings[type].set({
+        'primaryPattern': pattern,
+        'setting': setting,
+        'scope': (incognito ? 'incognito_session_only' : 'regular')
+      });
 }
+
+document.addEventListener('DOMContentLoaded', function () {
+  chrome.runtime.query({active: true, currentWindow: true}, function(tabs) {
+    var current = tabs[0];
+    incognito = current.incognito;
+    url = current.url;
+    var types = ['cookies', 'images', 'location', 'plugins',
+                 'popups', 'notifications', 'automaticDownloads'];
+    types.forEach(function(type) {
+      chrome.contentSettings[type] && chrome.contentSettings[type].get({
+            'primaryUrl': url,
+            'incognito': incognito
+          },
+          function(details) {
+            document.getElementById(type).disabled = false;
+            document.getElementById(type).value = details.setting;
+          });
+    });
+  });
+
+  var selects = document.querySelectorAll('select');
+  for (var i = 0; i < selects.length; i++) {
+    selects[i].addEventListener('change', settingChanged);
+  }
+});
